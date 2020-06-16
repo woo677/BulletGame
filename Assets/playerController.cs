1d84247ebe1f9448f358032d3e270d6b10c87fe7
woo677 <63435073+woo677@users.noreply.github.com>
2020-06-16 15:11:51 +0900

    30116정민우
    ...

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRigidbody;
     public float speed = 8f;
